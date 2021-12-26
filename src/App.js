4d69a73c4d1ad8d0d0c256674ebedf8dac4e1896
Jana Jolovic <janajolovic04@gmail.com>
2021-12-26 23:47:28 +0100

    state variables

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import WrongLetters from './components/WrongLetters';
 import Notification from './components/Notification';
 
 function App() {
+  const words = ['programming', 'javascript', 'react', 'hangman'];
+  let selectedWord = words[Math.floor(Math.random() * words.length)];
+  let playable = true;
+  const correctLetters = [];
+  const wrongLetters = [];
   return (
     <div>
       <Header />
