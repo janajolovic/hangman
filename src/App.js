0a3de2c54b0e40c826fc87d0d5c1c903d31cf11a
Jana Jolovic <janajolovic04@gmail.com>
2021-12-26 18:33:43 +0100

    creating components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './App.css';
 import Figure from './components/Figure';
 import Header from './components/Header';
+import Popup from './components/Popup';
+import Word from './components/Word';
+import WrongLetters from './components/WrongLetters';
+import Notification from './components/Notification';
 
 function App() {
   return (
     <div>
       <Header />
-      <Figure />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word />
+        <Popup />
+        <Notification />
+      </div>
     </div>
   );
 }
