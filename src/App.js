0339b12e4b41c24bb081c854e581f3e4f9d17052
Jana Jolovic <janajolovic04@gmail.com>
2021-12-27 19:31:04 +0100

    keydown handler

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Figure from './components/Figure';
 import Header from './components/Header';
@@ -13,6 +13,31 @@ function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
+
+  useEffect(() => {
+    const handleKeydown = event => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            // show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            // show(setShowNotification);
+          }
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeydown);
+  })
+
+  
   return (
     <div>
       <Header />
