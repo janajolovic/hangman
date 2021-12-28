13b93b5020fa468c0c4513025f35cdadb9fc9185
Jana Jolovic <janajolovic04@gmail.com>
2021-12-28 23:58:46 +0100

    popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { checkWin } from '../helpers/helpers';
 
 const Popup = ({correctLetters, wrongLetters, selectedWord, setPlayable}) => {
     let finalMessage = '';
     let finalMessageRevealWord = "";
     let playable = true;
+
+    if (checkWin(correctLetters, wrongLetters, selectedWord) === 'win') {
+        playable = false;
+        finalMessage = 'Congratulation! You won :)';
+    } else if (checkWin(correctLetters, wrongLetters, selectedWord) === "lose") {
+        finalMessage = 'You lost :(';
+        finalMessageRevealWord = `The word was ${selectedWord}`
+        playable = false;
+    }
+
+    useEffect(() => setPlayable(playable));
+
     return (
-        <div className='popup-container'>
+        <div className='popup-container' style={finalMessage === '' ? {display: flex} : {}}>
             <div className='popup'>
                 <h2>{finalMessage}</h2>
                 <h3>{finalMessageRevealWord}</h3>
